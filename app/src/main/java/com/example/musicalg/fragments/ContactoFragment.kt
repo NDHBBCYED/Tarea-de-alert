4af9880a54c98bffc6468320ae883a23badf7bcd
geronimoD18 <geronimoD18@github.com>
2023-06-07 12:15:05 -0400

    Music App

---
+++ app/src/main/java/com/example/musicalg/fragments/ContactoFragment.kt
@@ -0,0 +1,20 @@
+package com.example.musicalg.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.musicalg.R
+
+class ContactoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflar el diseño del fragmento
+        return inflater.inflate(R.layout.fragment_contacto, container, false)
+    }
+}
